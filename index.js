c480727687802bf517a42b76848bd7e40f45bf3b
Tomas Ortiz <TomasOrtiz@protonmail.com>
2019-11-14 14:29:54 -0800

    now on problem 5

--- index.js
+++ index.js
@@ -105,8 +105,16 @@ class Car {
         + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
         + {name} and {location} of course come from the instance's own properties.
 */
-class Lambdasian {}
-
+class Lambdasian {
+  constructor(argu) {
+    (this.name = argu.name),
+      (this.age = argu.age),
+      (this.location = argu.location);
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
 /*
   TASK 4
     - Write an Instructor class extending Lambdasian.
@@ -121,7 +129,20 @@ class Lambdasian {}
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {}
+class Instructor extends Lambdasian {
+  constructor(argu) {
+    super(argu);
+    this.specialty = argu.specialty;
+    this.favLanguage = argu.favLanguage;
+    this.catchphrase = argu.catchphrase;
+  }
+  demo(subject) {
+    return `Today we learn about ${subject}`;
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+}
 
 /*
   TASK 5
@@ -138,8 +159,19 @@ class Instructor {}
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {}
+class Student extends Lambdasian {
+  constructor(argu) {
+    super(argu);
 
+    this.previousBackground = argu.previousBackground;
+    this.className = arg.className;
+    this.favSubjects = [];
+  }
+  listSubjects() {
+    this.favSubjects.push(sub);
+    return `Loving HTML, CSS and JS!`;
+  }
+}
 /*
   TASK 6
     - Write a ProjectManager class extending Instructor.
